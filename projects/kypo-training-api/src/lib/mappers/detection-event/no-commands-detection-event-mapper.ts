f8ea2956b8244612c98d2fde706d8d34eafbb631
Lukáš <445519@mail.muni.cz>
2023-03-29 14:33:38 +0200

    Removed console.log().

--- projects/kypo-training-api/src/lib/mappers/detection-event/no-commands-detection-event-mapper.ts
+++ projects/kypo-training-api/src/lib/mappers/detection-event/no-commands-detection-event-mapper.ts
@@ -4,7 +4,6 @@ import { NoCommandsDetectionEventDTO } from '../../dto/detection-event/no-comman
 
 export class NoCommandsDetectionEventMapper {
   static fromDTO(dto: NoCommandsDetectionEventDTO): NoCommandsDetectionEvent {
-    console.log(dto);
     const result = new NoCommandsDetectionEvent();
     result.detectionEventType = AbstractDetectionEventTypeEnum.No_commands;
     return result;
